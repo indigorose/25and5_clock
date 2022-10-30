42f19490dd39460b4e6b616bf57fe326327b1c96
indigorose <indigorose049@gmail.com>
2022-10-30 09:02:25 +0000

    Complete css styling and layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlus,
+  faMinus,
+  faPlay,
+  faPause,
+  faRedo,
+} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  // useStates
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
   const [play, setPlay] = useState(false);
   const [timingType, setTimingType] = useState('SESSION');
   const [timeLeft, setTimeLeft] = useState(1500);
 
+  // timeout
   const timeout = setTimeout(() => {
     if (timeLeft && play) {
       setTimeLeft(timeLeft - 1);
     }
   }, 1000);
 
+  // Handle Functions
   const handleBreakIncrease = () => {
     if (breakLength < 60) {
       setBreakLength(breakLength + 1);
@@ -34,18 +45,10 @@ function App() {
   };
 
   const handleSessionDecrease = () => {
-    if (sessionLength > 25) {
+    if (sessionLength > 1) {
       setSessionLength(sessionLength - 1);
       setTimeLeft(timeLeft - 60);
     }
-  };
-
-  const timeFormatter = () => {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft - minutes * 60;
-    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
-    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   const handlePlay = () => {
@@ -56,6 +59,27 @@ function App() {
     // } else{
     //   setPlay(true)
     // }
+  };
+
+  const handleReset = () => {
+    clearTimeout(timeout);
+    setPlay(false);
+    setTimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimingType('SESSION');
+    const audio = document.getElementById('beep');
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  // Time Formatter
+  const timeFormatter = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   const resetTimer = () => {
@@ -76,82 +100,87 @@ function App() {
 
   const clock = () => {
     if (play) {
-      timeout();
+      // eslint-disable-next-line no-unused-expressions
+      timeout;
       resetTimer();
     } else {
       clearTimeout(timeout);
     }
   };
 
-  const handleReset = () => {
-    clearTimeout(timeout);
-    setPlay(false);
-    setTimeLeft(1500);
-    setBreakLength(5);
-    setSessionLength(25);
-    setTimingType('SESSION');
-    const audio = document.getElementById('beep');
-    audio.pause();
-    audio.currentTime = 0;
-  };
-
   useEffect(() => {
     clock();
-  }, [play, timeLeft, timeout, clock]);
-  // dummy function below - good idea for future projects
-  // const { handleReset } = {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [play, timeLeft, timeout]);
+
+  // dummy function below - acts like the "pass" in python for functions.
+  // const { play, handleBreakIncrease, handleBreakDecrease, handleSessionIncrease, handleSessionDecrease, timeFormatter, title, handlePlay, handleReset } = {};
 
   const title = timingType === 'SESSION' ? 'Session' : ' Break';
+
   return (
     <div className="App">
       <div className="container">
-        <h2>25 + 5 Clock</h2>
-        <div className="break-session-length">
-          <h3 id="break-label">Break Length</h3>
-          <button
-            disbaled={play}
-            onClick={handleBreakIncrease}
-            id="break-increment"
-          >
-            Increase
-          </button>
-          <p id="break-length">{breakLength}</p>
-          <button
-            disable={play}
-            onClick={handleBreakDecrease}
-            id="break-decrement"
-          >
-            Decrease
-          </button>
+        <div className="header-container">
+          <h2>25 + 5 Clock</h2>
         </div>
-        <div className="session-length-section">
-          <h3 id="session-label">session</h3>
-          <button
-            disbaled={play}
-            onClick={handleSessionIncrease}
-            id="session-increment"
-          >
-            increase
-          </button>
-          <p id="session-length">{sessionLength}</p>
-          <button
-            disable={play}
-            onClick={handleSessionDecrease}
-            id="session-decrement"
-          >
-            decrease
-          </button>
+        <div className="controls-container">
+          <div className="break-session-length">
+            <h3 id="break-label">Break Length</h3>
+            <div className="button-container">
+              <button
+                disbaled={play}
+                onClick={handleBreakIncrease}
+                id="break-increment"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+              <p id="break-length">{breakLength}</p>
+              <button
+                disabled={play}
+                onClick={handleBreakDecrease}
+                id="break-decrement"
+              >
+                <FontAwesomeIcon icon={faMinus} />
+              </button>
+            </div>
+          </div>
+          <div className="session-length-section">
+            <h3 id="session-label">Session Length</h3>
+            <div className="button-container">
+              <button
+                disbaled={play}
+                onClick={handleSessionIncrease}
+                id="session-increment"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+              <p id="session-length">{sessionLength}</p>
+              <button
+                disabled={play}
+                onClick={handleSessionDecrease}
+                id="session-decrement"
+              >
+                <FontAwesomeIcon icon={faMinus} />
+              </button>
+            </div>
+          </div>
         </div>
         <div className="timer-container">
-          <h2 id="timer-label">{title}</h2>
-          <h3 id="time-left">{timeFormatter()}</h3>
+          <div className="timer">
+            <h2 id="timer-label">{title}</h2>
+            <p id="time-left">{timeFormatter()}</p>
+          </div>
+          <div className="timer-buttons">
+            <button onClick={handlePlay} id="start_stop">
+              <FontAwesomeIcon icon={faPlay} /> /{' '}
+              <FontAwesomeIcon icon={faPause} />
+            </button>
+            <button onClick={handleReset} id="reset">
+              <FontAwesomeIcon icon={faRedo} />
+            </button>
+          </div>
         </div>
-        <button onClick={handlePlay} id="start_stop">
-          Start/Stop
-        </button>
-        <button onClick={handleReset} id="reset">
-          Reset
-        </button>
       </div>
       <audio
         id="beep"
@@ -161,61 +190,5 @@ function App() {
     </div>
   );
 }
-
-// User Story #1: I can see an element with id="break-label" that contains a string (e.g. "Break Length").
-
-// User Story #2: I can see an element with id="session-label" that contains a string (e.g. "Session Length").
-
-// User Story #3: I can see two clickable elements with corresponding IDs: id="break-decrement" and id="session-decrement".
-
-// User Story #4: I can see two clickable elements with corresponding IDs: id="break-increment" and id="session-increment".
-
-// User Story #5: I can see an element with a corresponding id="break-length", which by default (on load) displays a value of 5.
-
-// User Story #6: I can see an element with a corresponding id="session-length", which by default displays a value of 25.
-
-// User Story #7: I can see an element with a corresponding id="timer-label", that contains a string indicating a session is initialized (e.g. "Session").
-
-// User Story #8: I can see an element with corresponding id="time-left". NOTE: Paused or running, the value in this field should always be displayed in mm:ss format (i.e. 25:00).
-
-// User Story #9: I can see a clickable element with a corresponding id="start_stop".
-
-// User Story #10: I can see a clickable element with a corresponding id="reset".
-
-// User Story #11: When I click the element with the id of reset, any running timer should be stopped, the value within id="break-length" should return to 5, the value within id="session-length" should return to 25, and the element with id="time-left" should reset to its default state.
-
-// User Story #12: When I click the element with the id of break-decrement, the value within id="break-length" decrements by a value of 1, and I can see the updated value.
-
-// User Story #13: When I click the element with the id of break-increment, the value within id="break-length" increments by a value of 1, and I can see the updated value.
-
-// User Story #14: When I click the element with the id of session-decrement, the value within id="session-length" decrements by a value of 1, and I can see the updated value.
-
-// User Story #15: When I click the element with the id of session-increment, the value within id="session-length" increments by a value of 1, and I can see the updated value.
-
-// User Story #16: I should not be able to set a session or break length to <= 0.
-
-// User Story #17: I should not be able to set a session or break length to > 60.
-
-// User Story #18: When I first click the element with id="start_stop", the timer should begin running from the value currently displayed in id="session-length", even if the value has been incremented or decremented from the original value of 25.
-
-// User Story #19: If the timer is running, the element with the id of time-left should display the remaining time in mm:ss format (decrementing by a value of 1 and updating the display every 1000ms).
-
-// User Story #20: If the timer is running and I click the element with id="start_stop", the countdown should pause.
-
-// User Story #21: If the timer is paused and I click the element with id="start_stop", the countdown should resume running from the point at which it was paused.
-
-// User Story #22: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-label should display a string indicating a break has begun.
-
-// User Story #23: When a session countdown reaches zero (NOTE: timer MUST reach 00:00), a new break countdown should begin, counting down from the value currently displayed in the id="break-length" element.
-
-// User Story #24: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), and a new countdown begins, the element with the id of timer-label should display a string indicating a session has begun.
-
-// User Story #25: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), a new session countdown should begin, counting down from the value currently displayed in the id="session-length" element.
-
-// User Story #26: When a countdown reaches zero (NOTE: timer MUST reach 00:00), a sound indicating that time is up should play. This should utilize an HTML5 audio tag and have a corresponding id="beep".
-
-// User Story #27: The audio element with id="beep" must be 1 second or longer.
-
-// User Story #28: The audio element with id of beep must stop playing and be rewound to the beginning when the element with the id of reset is clicked.
 
 export default App;
